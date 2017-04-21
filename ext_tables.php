5b5c6d8ab61c7a29d33cf00e15136e631b312ce2
Sacha Vorbeck <Sacha.Vorbeck@devolo.de>
2017-04-21 16:45:36 +0200

    [FIX] changed extension references

--- ext_tables.php
+++ ext_tables.php
@@ -6,12 +6,6 @@ if (!defined('TYPO3_MODE')) {
 
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
     $_EXTKEY,                                       // Extension Key
-    'Configuration/TypoScript/CssStyledContent',    // Path to setup.txt and constants.txt
-    'Example Package - CSS Styled Content'          // Title in the selector box
-);
-
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
-    $_EXTKEY,                                       // Extension Key
     'Configuration/TypoScript/FluidStyledContent',  // Path to setup.txt and constants.txt
-    'Example Package - Fluid Styled Content'        // Title in the selector box
+    'Site Package - Fluid Styled Content'        // Title in the selector box
 );
